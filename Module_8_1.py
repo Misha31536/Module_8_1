29c000b1dc6a1adae6bc65bb77666668eed88b4f
Misha31536 <e4ik@ya.ru>
2024-08-08 10:31:24 +0700

    2024/08/08

---
+++ Module_8_1.py
@@ -0,0 +1,16 @@
+
+def add_everything_up(a, b):
+    try:
+        sum_ = a + b
+        return sum_
+    except TypeError:
+        if isinstance(a, int) or isinstance(a, float) :
+            a = str(a)
+        else:
+            b = str(b)
+        sum_ = a + b
+        return sum_
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
